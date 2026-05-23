aff7389f022ae0d4a20746527b2577ca253f8ef4
Maxime Bourret <maxime.bourret@hey.com>
2026-05-23 19:48:02 -0400

    added game_map

--- actions.py
+++ actions.py
@@ -1,9 +1,29 @@
-class Action:
-    pass
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
+class Action(ABC):
+    @abstractmethod
+    def perform(self, engine: Engine, entity: Entity):
+        """Perform this action with the objects needed to determine its scope.
+
+        `engine` is the scope this action is being performed in.
+
+        `entity` is the object performing the action.
+
+         This method must be overridden by Action subclasses.
+        """
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity):
+        raise SystemExit()
 
 
 class MovementAction(Action):
@@ -11,3 +31,15 @@ class MovementAction(Action):
         super().__init__()
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity):
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is not in bounds
+
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is not walkable
+
+        entity.move(self.dx, self.dy)
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import tcod
 
-from actions import MovementAction, EscapeAction
+from engine import Engine
+import game_map
 from input_handlers import EventHandler
+from entity import Entity
+from game_map import GameMap
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
-
+    map_width = 80
+    map_height = 45
     tileset = tcod.tileset.load_tilesheet("ascii.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
-
     event_handler = EventHandler()
+    player = Entity(screen_width // 2, screen_height // 2, "@", (255, 255, 255))
+    npc = Entity(screen_width // 2 - 5, screen_height // 2 - 5, "@", (255, 255, 0))
+    entites = {npc, player}
+    game_map = GameMap(map_width, map_height)
+    engine = Engine(entites, event_handler, game_map, player)
 
     with tcod.context.new(
         width=screen_width,
@@ -24,23 +29,8 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-
-            context.present(root_console)
-
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            engine.handle_events(tcod.event.wait())
 
 
 if __name__ == "__main__":
